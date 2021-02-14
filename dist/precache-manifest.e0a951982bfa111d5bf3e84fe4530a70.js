78a132a8cc2523786f1e05b4e4dc0ca43f4e9a49
Sathish Kumar <30808898+raasalgul@users.noreply.github.com>
2021-02-14 16:14:48 +0530

    Add files via upload

---
+++ dist/precache-manifest.e0a951982bfa111d5bf3e84fe4530a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a536991e32486485d0e4a15247a76525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0580aeb64093090b21e7",
+    "url": "/static/css/main.4a2d28a0.chunk.css"
+  },
+  {
+    "revision": "12ce8a74a3efc3f06d05",
+    "url": "/static/js/2.657b1a3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.657b1a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0580aeb64093090b21e7",
+    "url": "/static/js/main.00786693.chunk.js"
+  },
+  {
+    "revision": "f5f555c8e247b214e71c",
+    "url": "/static/js/runtime-main.e71a846f.js"
+  }
+]);
---
+++ public/init/init.js
@@ -0,0 +1,129 @@
+// const xl = require('excel4node');
+function init() {
+  console.log("Initializing....");
+  var link = document.createElement('link');
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('type', 'text/css');
+  link.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Special+Elite&display=swap');
+  document.head.appendChild(link);
+  var inputs = getInputsByValue();
+  const div = document.createElement('div');
+  var finalHTML = '<div id="cheta-flt-dv"><p class="cheta-flt-p">ChETA</p>';
+  finalHTML += '<p class="cheta-pfnt">Words: <span id="cheta-data-wordcount">'+inputs+'</span></p>';
+  finalHTML += '</div>';
+  div.innerHTML = finalHTML;
+  document.body.appendChild(div);
+}
+
+function getInputsByValue()
+{
+    let excelData=[];
+    let issueList = document.getElementsByClassName("issue-list")[0].innerText;
+    console.log(`issueList ${issueList}`);
+    let totalIssues=document.getElementsByClassName("showing")[0]
+    .getElementsByTagName("span")[0].innerText.split(" of ")[1];
+    console.log(`issueList total count ${totalIssues}`);
+    for(let i=0;i<totalIssues;i++)
+    {
+     task(i,totalIssues);
+    }
+    // const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    // const fileExtension = '.xlsx';
+
+    // const exportToCSV = (csvData, fileName) => {
+    //     const ws = XLSX.utils.json_to_sheet(csvData);
+    //     const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
+    //     const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    //     const data = new Blob([excelBuffer], {type: fileType});
+    //     FileSaver.saveAs(data, fileName + fileExtension);
+    // }
+    // exportToCSV(excelData,"prod_data");
+
+//     const wb = new xl.Workbook();
+//     const ws = wb.addWorksheet('Worksheet Name');
+//     const headingColumnNames = [
+//       "email","status","totalSalesForces",
+//       "description","comments"
+//   ]
+//   let headingColumnIndex = 1;
+//   headingColumnNames.forEach(heading => {
+//       ws.cell(1, headingColumnIndex++)
+//           .string(heading)
+//   });
+//   let rowIndex = 2;
+//   excelData.forEach( record => {
+//     let columnIndex = 1;
+//     Object.keys(record ).forEach(columnName =>{
+//         ws.cell(rowIndex,columnIndex++)
+//             .string(record [columnName])
+//     });
+//     rowIndex++;
+// }); 
+// wb.write('prod-data.xlsx');
+// wb.SheetNames.push("Test Sheet");
+
+
+// var wb = XLSX.utils.book_new();
+// wb.Props = {
+//         Title: "SheetJS Tutorial",
+//         Subject: "Test",
+//         Author: "Red Stapler",
+//         CreatedDate: new Date(2017,12,19)
+// };
+// var ws = XLSX.utils.aoa_to_sheet(excelData);
+// wb.Sheets["Test Sheet"] = ws;
+// var wbout = XLSX.write(wb, {bookType:'xlsx',  type: 'binary'});
+//   saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'test.xlsx');
+// function s2ab(s) { 
+//   var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+//   var view = new Uint8Array(buf);  //create uint8array as viewer
+//   for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+//   return buf;    
+// }
+
+    function task(i,totalIssues) { 
+      setTimeout(function() { 
+        let email=document.getElementsByClassName("aui-avatar aui-avatar-small")[1]
+        .getElementsByTagName("img")[0].getAttribute("alt");
+        console.log(`email ${email}`);
+        let status=document.getElementsByClassName("wrap")[1]
+        .getElementsByTagName("span")[0].getElementsByTagName("span")[0].innerText;
+        console.log(`status ${status}`);
+        let totalSalesForce=document.getElementsByClassName("wrap")[9]
+        .getElementsByTagName("a");
+        let totalSalesForces="";
+        for(let i=0;i<totalSalesForce.length;i++)
+        {
+          console.log(`Sales force ${totalSalesForce[i].getAttribute("href")}`);
+          totalSalesForces+="\n\n"+totalSalesForce[i].getAttribute("href");
+        }
+        let description=document.getElementsByClassName("user-content-block")[0]
+        .getElementsByTagName("p")[0].innerText;
+        console.log(`description ${description}`);
+        let allComments=document.getElementsByClassName("issuePanelContainer")[0]
+        .getElementsByClassName("action-body");
+        let comments="";
+        for(let i=0;i<allComments.length;i++)
+        {
+          let comment="";
+          let paragraphs=allComments[i].getElementsByTagName("p");
+          for(let j=0;j<paragraphs.length;j++)
+          {
+            comment+=" "+paragraphs[j].innerText;
+          }
+          console.log(`comment ${i+1} ${comment}`);
+          comments+="\n\n"+comment;
+        }
+        if(i+1!=totalIssues)
+        {
+        document.getElementsByClassName("splitview-issue-link")[i+1].click();
+        }
+        console.log(`-----------------------------------------------------------------`); 
+        excelData.push({"email":email,"status":status,"totalSalesForces":totalSalesForces,
+          "description":description,"comments":comments});
+      }, 9000 * i); 
+    } 
+    return '<h1>satz</h1>';
+}
+
+init();
